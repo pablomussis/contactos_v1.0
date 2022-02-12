6de14283798d8d4bc700f91478fa73989d9551b8
Pablo Mussis <mussisp@gmail.com>
2022-02-11 22:20:16 -0300

    Metiendo arreglos ..

--- main.py
+++ main.py
@@ -2,43 +2,53 @@
 # -*- coding: utf-8 -*-
 
 '''
-Se libera el siguiente código para ser estudiado, reutilizado y redistribuido con sus modificaciones.
-	- Migración de Python 2.7 a la versión 3.7.3.
-	- Marzo de 2019 Versión 0.1 - Prof. Mussis Pablo
-	- Script en Python 3.7.3 como A. B. M. con base de datos en SQlite3
-	- sobre el sistema de base GNU/Debian 10 en estado de prototipo V0.1
-	- Desarrollado en el laboratorio de la E.E.S.T.N°2 de Fcio Varela
-	- Con el propósito práctico profesional.
+ - Migración de Python 2.7 a la versión 3.7.3.
+ - Marzo de 2019 Versión 1.0
+ - U.I. en C.L.I. - Script en Python 3.7.3 como A. B. M. con base de datos en SQlite3
+ - Sobre el sistema de base GNU/Debian 10.
+ - PABLO MUSSIS
 
-Nota, para su próxima versión se requiere: 
-	- Verificar el sistema operativo base para ejecutar el comando clear en equipos UNIX o cls en M.Windows.
-	- Modularizar tareas.
-	- Mejorar la salida de consulta en la opción 1.
+
+Nota: 
+	OK - Verificar el sistema operativo base para ejecutar el comando clear en equipos UNIX o cls en M.Windows.
+	OK 70% - Modularizar tareas.
+	X - Mejorar la salida de consulta en la opción 1.
 '''
 
-import sqlite3
+import limpiar as lp						# Módulo personal para limpiar la terminal UNIX o simbolo de sistemas de M.W.
+import ui									# Módulo personal que imprime la U.I.: Interfaz de Usuario.
+#import conexion as con						# Módulo personal que carga y pone activa la base de datos en SQLite.
+
 import sys
 import os
 
+import sqlite3
+
+
 class contacto:
 
-	def limpiar_pantalla(self) :
-		os.system('clear')	# Limpia la pantalla | en Windows el comando es 'cls'
+	#def limpiar_pantalla(self) :
+	#	os.system('clear')	# Limpia la pantalla | en Windows el comando es 'cls'
+	
+	lp.Pantalla()							# Limpia la pantalla (clear o cls), determina sobre que sistema operativo está corriendo el script.
+	ui.ImprimirMenu()						# Imprime la UI
 
+	
 	# Función que imprime el menu a seleccionar por el usuario:
-	def imprimir_menu(self) :
-		# Ingresamos una cadena de caracteres a la variable local menu:
-		menu = '''SELECCIONE UNA OPCIÓN: \n\n
-				1. CONSULTA
-				2. MODIFICACIÓN
-				3. ALTA
-				4. BAJA
-				5. SALIR \n\n'''
+	#def imprimir_menu(self) :
+		# Ingresamos una cadena de caracteres a la variable local menu:		
+	#	menu = '''LISTADO DE CONTACTOS: \n\n
+	#			SELECCIONE UNA OPCIÓN: \n\n
+	#			1. CONSULTA
+	#			2. MODIFICACIÓN
+	#			3. ALTA
+	#			4. BAJA
+	#			5. SALIR \n\n'''
 
-		print ('\n' * 2, '\t' * 4, ('=' * 50), '\n' * 2)
-		print ('\t' * 4, menu)
-		print ('\t' * 4, ('=' * 50))
-
+	#	print ('\n' * 2, '\t' * 4, ('=' * 50), '\n' * 2)
+	#	print ('\t' * 4, menu)
+	#	print ('\t' * 4, ('=' * 50))
+	
 
 	# Función que carga y pone activa la base de datos (contacto.db) con su tabla (contactos):
 	def cargar_base(self) :
@@ -49,18 +59,22 @@ class contacto:
 		# Con cursor.execute ejecutaremos sentencias de SQLite3 y SQL
 		self.cursor.execute("CREATE TABLE IF NOT EXISTS contactos (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT NOT NULL, contacto TEXT NOT NULL)")
 
+	#con.cargarBase()
+
 	def leer_opcion(self) :		# Función que COMPRUEBA VALIDACION DE DATOS de entrada
 		while True :
 			try :
 				self.opcion = int(input('\n\t\t\t\tINGRESE UNA OPCIÓN: '))
-				self.limpiar_pantalla()
+				#self.limpiar_pantalla()
+				lp.Pantalla()
 				if (self.opcion < 6) :
 					break
 			except ValueError :
 				print ('\n', '\t' * 4, 'INTENTE NUEVAMENTE..!')
 
 	def comprobar_opcion(self) :	# Función que COMPRUEBA OPCION de menu
-		self.limpiar_pantalla()
+		lp.Pantalla()
+		#self.limpiar_pantalla()
 
 		if (self.opcion == 1) :
 			print ('\n', '\t' * 4, 'CONSULTA DE REGISTROS: \n')
@@ -102,6 +116,7 @@ class contacto:
 					tupla_registro = (contacto, id)
 					self.cursor.execute("UPDATE contactos SET contacto = ? WHERE id = ?", tupla_registro)
 				self.actualizar_base()
+				#con.actualizarBase()
 				print ('\n', '\t' * 4, 'LOS DATOS SE MODIFICARON CORRECTAMENTE..!\n')
 			else:
 				print ('\n', '\t' * 4, 'NO HAY REGISTROS A MODIFICAR..!')
@@ -114,9 +129,10 @@ class contacto:
 			# Tupla (alta) que contiene los nuevos datos:
 			alta = (nombre, contacto)
 
-			# Inserta y actualiza (función actualizar_base()) los nuevos registros de la base:
+			# Inserta y actualiza (función actualizarBase()) los nuevos registros de la base:
 			self.cursor.execute("INSERT INTO contactos (nombre, contacto) VALUES (?, ?)", alta)
 			self.actualizar_base()
+			#con.actualizarBase()
 			print ('\n', '\t' * 4, 'LOS DATOS SE INSERTARON CORRECTAMENTE..!\n')
 
 
@@ -129,34 +145,43 @@ class contacto:
 			self.cursor.execute("SELECT * FROM contactos WHERE id = %d" %(id))
 
 			if (self.cursor.fetchone()) :
-				# Elimina y actualiza (función actualizar_base()) el registro de la base:
+				# Elimina y actualiza (función actualizarBase()) el registro de la base:
 				self.cursor.execute("DELETE FROM contactos WHERE id = %d" %(id))
 				self.actualizar_base()
+				#con.actualizarBase()
 				print ('\n', '\t' * 4, 'LOS DATOS SE ELIMINARON CORRECTAMENTE..!\n')
 			else :
 				print ('\n', '\t' * 4, 'NO HAY REGISTRO A DAR DE BAJA\n')
 
 
 		elif (self.opcion == 5) :
-			self.limpiar_pantalla()
+			#self.limpiar_pantalla()
+			lp.Pantalla()
 			self.cerrar_base()
+			#con.cerrarBase()
 
-		self.imprimir_menu()
+		ui.ImprimirMenu()
 		self.leer_opcion()
 		self.comprobar_opcion()
 
 	def actualizar_base(self) :
 		self.conexion.commit()
+	#con.actualizarBase()
 
 	def cerrar_base(self) :
 		self.conexion.close()
 		exit()
+	#con.cerrarBase()
 
 	def __init__(self) :
-		self.limpiar_pantalla()
-		self.imprimir_menu()
+		#self.limpiar_pantalla()
+		lp.Pantalla()
+		#self.imprimir_menu()
+		ui.ImprimirMenu()
 		self.cargar_base()
+		#con.cargarBase()
 		self.leer_opcion()
 		self.comprobar_opcion()
 
 contactos = contacto()	# La clase contacto() INSTANCIA al objeto menu
+
